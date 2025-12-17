2a2918bfda1362398eed80631be7f92ed1433943
GAMMING-SERVICE  [SYSTEM NODE-01] <167754757+Gamming-SERVICE@users.noreply.github.com>
2025-12-18 00:38:29 +0530

    Add navigation bar to nav.php

---
+++ nav.php
@@ -0,0 +1,7 @@
+<div class="nav">
+<a href="dashboard.php">Dashboard</a>
+<a href="users.php">Users</a>
+<a href="subdomains.php">Subdomains</a>
+<a href="dns.php">DNS</a>
+<a href="logout.php">Logout</a>
+</div>
